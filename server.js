323709c05df07169a50aecccd67dcdb975204769
teaonrocks <arxherchua@gmail.com>
2026-01-19 10:27:36 +0800

    fix: cors again

--- server.js
+++ server.js
@@ -8,21 +8,7 @@ const allowedOrigins = [
 // "https://YOUR-frontend.vercel.app", // add later
 // "https://YOUR-frontend.onrender.com" // add later
 ];
-app.use(
-cors({
-origin: function (origin, callback) {
-// allow requests with no origin (Postman/server-to-server)
-if (!origin) return callback(null, true);
-if (allowedOrigins.includes(origin)) {
-return callback(null, true);
-}
-return callback(new Error("Not allowed by CORS"));
-},
-methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-allowedHeaders: ["Content-Type", "Authorization"],
-credentials: false,
-})
-);
+
 
 
 const dbConfig = {
@@ -96,3 +82,19 @@ app.put("/updatecard/:card_id", async (req, res) => {
 		console.error(error);
 	}	
 });
+
+app.use(
+	cors({
+	origin: function (origin, callback) {
+	// allow requests with no origin (Postman/server-to-server)
+	if (!origin) return callback(null, true);
+	if (allowedOrigins.includes(origin)) {
+	return callback(null, true);
+	}
+	return callback(new Error("Not allowed by CORS"));
+	},
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+	credentials: false,
+	})
+	);
